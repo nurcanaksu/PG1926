d427ba545ac6bb5f92f2e03443a7bd083ab1d170
nurcanaksu <63795619+nurcanaksu@users.noreply.github.com>
2021-01-31 16:35:40 +0300

    php odev1

--- index.php
+++ index.php
@@ -1,19 +1,19 @@
-<html xmlns="http://www.w3.org/1999/xhtml">
+<?php
 
-<head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <title>Para Üstü</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-</head>
+echo "<br>";
+$saat=strtotime("21:15");
 
-<body>
-    <form action="ikinci.php" method="post">
-        <h1> PARA ÜSTÜ HESAPLAMA</h1>
-        <h6>Lütfen para üstünü kuruş cinsinden giriniz:</h6>
-        <input name="para" type="text" />
-        <input name="gonder" type="submit" value="Para Üstünü Göster" />
-        <br />
-    </form>
-</body>
+if($saat>strtotime("06:00") and $saat< strtotime("10:00")  )
+{
+    echo "<br>";
+    echo "GÜNAYDIN !! <br>";
+}else if($saat>strtotime("10:00") and $saat< strtotime("17:00")){
 
-</html>
+    echo "İYİ GÜNLER !!";
+}else if($saat>strtotime("17:00") and $saat< strtotime("20:00"))
+{
+    echo "İYİ AKŞAMLAR!";
+}
+else
+   echo "İYİ GECELER!";
+?>
